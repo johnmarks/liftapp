4bb454d604ba9fa5e7886962b9fd7c3c0636aaee
John Marks <john@bluefroggaming.com>
2014-11-13 16:45:09 -0500

    add root directory

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var statik = require('statik');
-var server = statik('app');
-server.listen({
+// var server = statik('app');
+statik({
   port: process.env.PORT || 5000,
   root: '/app'
 });

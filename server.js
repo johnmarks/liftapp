ba8d6467518c361073556c46c2615ac4243ec50f
John Marks <john@bluefroggaming.com>
2014-11-13 16:26:52 -0500

    another try at the server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
 
 var statik = require('statik');
-var server = statik.createServer('.', {default: "app/index.html"});
+var server = statik.createServer('app', {default: "index.html"});
 server.listen();
